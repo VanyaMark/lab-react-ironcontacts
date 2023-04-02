14efb5f74cda3d12d8936984a58f727d82daee0d
Vanya Mark <markova.vanya@gmail.com>
2023-04-02 16:46:30 +0200

    got rid of contactsArr but sort by popularity still not working

--- src/App.js
+++ src/App.js
@@ -3,38 +3,30 @@ import "./App.css";
 import contacts from "./contacts.json";
 
 function App() {
-  const [ContactList, setContactList] = useState([])
   const contactsArr = contacts.slice(0, 5);
+  const [ContactList, setContactList] = useState(contactsArr)
   console.log('ContactList ', ContactList);
-  console.log('contactsArr: ', contactsArr)
 
   const addNewContact = () => {
     const index=Math.floor(Math.random() * (contacts.length - 5)) + 5;
     const newContact = contacts[index]
     console.log('newContact: ', newContact)
-    const newContactList = [...contactsArr, newContact];
+    const newContactList = [...ContactList, newContact];
     console.log('newContactList: ', newContactList)
     setContactList(newContactList)
   }
 
   const sortByPopularity = () => {
-     if (ContactList.length > 0) {
       const sortedByPopularity = ContactList.sort(function(c1, c2) {
         if (c1.popularity < c2.popularity) {
           return 1;
         } else {return -1}
       })
       console.log('sorted: ', sortedByPopularity)
-    } else {
-      const sortedByPopularity = contactsArr.sort(function(c1, c2) {
-        if (c1.popularity < c2.popularity) {
-          return 1;
-        } else {return -1}
-      })
-      console.log('sorted: ', sortedByPopularity)
       setContactList(sortedByPopularity)
+      console.log('ContactList sorted: ', ContactList)
+      return sortedByPopularity;
     }
-  }
 
 
   return (
@@ -45,38 +37,7 @@ function App() {
       <button onClick={sortByPopularity}>Sort by popularity</button>
       <button onClick={addNewContact}>Sort by name</button>
       </div>
-      {ContactList.length === 0 ? <table>
-        <thead>
-          <tr>
-            <th>Picture</th>
-            <th>Name</th>
-            <th>Popularity</th>
-            <th>Won Oscar</th>
-            <th>Won Emmy</th>
-          </tr>
-        </thead>
-        {contactsArr.map((contact) => (
-          <tbody key={contact.id}>
-            <tr>
-              <td>
-                <img
-                  className="profilePic"
-                  src={contact.pictureUrl}
-                  alt={contact.name}
-                />
-              </td>
-              <td>
-                {contact.name}
-              </td>
-              <td>
-                {contact.popularity.toFixed(2)}
-              </td>
-              {!contact.wonOscar ? <td>🥺</td> : <td>🏆</td>}
-              {!contact.wonEmmy ? <td>🥺</td> : <td>🏆</td>}
-            </tr>
-          </tbody>
-        ))}
-      </table> : <table>
+<table>
         <thead>
           <tr>
             <th>Picture</th>
@@ -107,7 +68,7 @@ function App() {
             </tr>
           </tbody>
         ))}
-      </table> }  
+      </table>
     </div>
   );
 }
